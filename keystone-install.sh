20f9134f454d34210dbaed7038ff03f97b3b7bd2
Eric Wright <eric@discoposse.com>
2013-07-08 20:44:39 -0400

    added silent install for ppa

--- keystone-install.sh
+++ keystone-install.sh
@@ -12,8 +12,8 @@ echo "mysql-server-5.5 mysql-server/root_password_again seen true" | sudo debcon
         
 sudo apt-get update
 sudo apt-get install ntp python-software-properties -y
-sudo add-apt-repository ppa:openstack-ubuntu-testing/grizzly-trunk-testing
-sudo add-apt-repository ppa:openstack-ubuntu-testing/grizzly-build-depends
+sudo add-apt-repository -y ppa:openstack-ubuntu-testing/grizzly-trunk-testing
+sudo add-apt-repository -y ppa:openstack-ubuntu-testing/grizzly-build-depends
 sudo apt-get update && apt-get upgrade -y
 
 sudo apt-get -y install mysql-server python-mysqldb
