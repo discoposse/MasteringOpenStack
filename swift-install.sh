2a7cfcb0d6922553b7aa20ee75af81edd823c091
Eric Wright <eric@discoposse.com>
2013-07-31 14:35:39 -0400

    updated swift-install.sh
    added cloud keyring updates and other pre-requisites

--- swift-install.sh
+++ swift-install.sh
@@ -1,8 +1,16 @@
 # Create a physical volume for use later
 pvcreate /dev/sdb
+useradd -p swift swift
 
-# Install some packages:
-sudo apt-get -y install swift swift-proxy swift-account swift-container swift-object memcached xfsprogs curl python-webob python-swiftclient
+sudo apt-get update
+sudo apt-get install -y ubuntu-cloud-keyring
+sudo echo "deb http://ubuntu-cloud.archive.canonical.com/ubuntu precise-updates/grizzly main" | sudo tee /etc/apt/sources.list.d/grizzly.list
+sudo apt-get install python-software-properties -y
+sudo add-apt-repository -y ppa:openstack-ubuntu-testing/grizzly-trunk-testing
+sudo add-apt-repository -y ppa:openstack-ubuntu-testing/grizzly-build-depends
+sudo apt-get update && apt-get upgrade -y
+
+sudo apt-get install -y swift swift-proxy swift-account swift-container swift-object memcached xfsprogs curl python-webob python-swiftclient
 sudo service ntp restart
 
 # Create signing directory & Set owner to swift
