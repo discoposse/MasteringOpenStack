578f4aa73eb8b8eb56bb6314661eff6fd5f0afa2
Eric Wright <eric@discoposse.com>
2013-08-03 22:37:30 -0400

    updated cinder script with correct MySQL credentials

--- cinder-install.sh
+++ cinder-install.sh
@@ -4,7 +4,7 @@ export MYSQL_PASS=openstack
 
 mysql -uroot -p$MYSQL_ROOT_PASS -e "CREATE DATABASE cinder;"
 
-mysql -uroot -p$MYSQL_ROOT_PASS -e "GRANT ALL ON keystone.* TO 'cinder'@'localhost' IDENTIFIED BY '$MYSQL_PASS';"
+mysql -uroot -p$MYSQL_ROOT_PASS -e "GRANT ALL ON cinder.* TO 'cinder'@'localhost' IDENTIFIED BY '$MYSQL_PASS';"
 
 sudo apt-get install -y cinder-api cinder-scheduler cinder-volume open-iscsi iscsitarget-dkms python-cinderclient tgt rabbitmq-server linux-headers-`uname -r`
 
@@ -14,7 +14,14 @@ sudo sed -i 's/false/true/g' /etc/default/iscsitarget
 service iscsitarget start
 service open-iscsi start
 
-sudo sed -i 's#^connection.*#connection = mysql://cinder:cinder@127.0.0.1/glance#' /etc/cinder/cinder.conf
+
+
+
+#
+sudo sed -i 's#^connection.*#connection = mysql://cinder:openstack@127.0.0.1/cinder#' /etc/cinder/cinder.conf
+# add the rabbit_password line
+
+
 sudo sed -i 's#^admin_tenant_name.*#admin_tenant_name = service#' /etc/cinder/cinder.conf
 sudo sed -i 's#^admin_user.*#admin_user = cinder#' /etc/cinder/cinder.conf
 sudo sed -i 's#^admin_password.*#admin_password = cinder#' /etc/cinder/cinder.conf
@@ -30,7 +37,7 @@ cinder-manage db sync
 
 service cinder-api restart
 service cinder-scheduler restart
-service conder-volume restart
+service cinder-volume restart
 
 
 
